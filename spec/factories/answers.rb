f1829dd0d09fe6272e8acdf7242559ab57caec63
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2017-03-09 01:01:22 +0300

    Answer tests: belongs to question, body pressence and max length

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :answer do
+    question_id 1
     title "MyString"
     body "MyText"
   end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:body) }
+  describe 'Associations' do
+    describe 'Question' do
+      it { should belong_to(:question) }
+      it { should validate_presence_of(:question) }
+    end
+  end
+
+  describe 'Validations' do
+    describe 'Body' do
+      it { should validate_presence_of(:body) }
+      it { should validate_length_of(:body).is_at_most(1000) }
+    end
+  end
 end
