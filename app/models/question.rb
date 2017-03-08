7fdd67e6ee2422d434ba776b59862c534782a7eb
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2017-03-09 01:03:42 +0300

    Refactor question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,11 @@
 class Question < ApplicationRecord
-  validates :title, presence: true
-  validates :body, presence: true
+  has_many :answers, dependent: :destroy
+
+  validates :title,
+            presence: true,
+            length: { maximum: 150 }
+
+  validates :body,
+            presence: true,
+            length: { maximum: 1000 }
 end
