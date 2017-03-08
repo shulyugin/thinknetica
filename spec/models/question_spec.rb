6c5eacc7fd6d3cc06be10e9c5ecd6d574ddec71a
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2017-03-09 01:02:00 +0300

    Question spec refactored

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:body) }
+  describe 'Associations' do
+    describe 'Answer' do
+      it { should have_many(:answers).dependent(:destroy) }
+    end
+  end
+
+  describe 'Validations' do
+    describe 'Title' do
+      it { should validate_presence_of(:title) }
+      it { should validate_length_of(:title).is_at_most(150) }
+    end
+    describe 'Body' do
+      it { should validate_presence_of(:body) }
+      it { should validate_length_of(:body).is_at_most(1000) }
+    end
+  end
 end
