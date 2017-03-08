6b9ace745ab6ecd2a412cf7326ee7e24f6433775
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2017-03-09 01:49:16 +0300

    Remove unnecessary presence validation of Question in Answer (‘cause it already checked in belongs_to)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  validates_presence_of :question
-
   validates :body,
             presence: true,
             length: { maximum: 1000 }
