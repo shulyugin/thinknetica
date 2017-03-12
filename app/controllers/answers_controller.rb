e6a6f05934d358258337278f382c6d650a1a018b
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2017-03-13 02:28:40 +0300

    Add answer controller

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,53 @@
+class AnswersController < ApplicationController
+  before_action :load_question, only: [:index, :create, :new]
+  before_action :load_answer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @answers = @question.answers
+  end
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  def show; end
+
+  def edit; end
+
+  def create
+    @answer = @question.answers.create(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @answer.update(answer_params)
+      redirect_to @answer
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @answer.destroy
+    redirect_to question_answers_path(@answer.question)
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def load_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+end
