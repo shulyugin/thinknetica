ddf3f82ddab13bf2b1383706d398ea2de4dda45b
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2017-03-13 02:20:35 +0300

    Refactor models specs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,16 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   describe 'Associations' do
-    describe 'Question' do
-      it { should belong_to(:question) }
-      it { should validate_presence_of(:question) }
-    end
+    it { should belong_to(:question) }
   end
 
   describe 'Validations' do
-    describe 'Body' do
-      it { should validate_presence_of(:body) }
-      it { should validate_length_of(:body).is_at_most(1000) }
-    end
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_most(1000) }
   end
 end
