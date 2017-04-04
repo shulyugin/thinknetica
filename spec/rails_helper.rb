0d4999ba93c54a36fb531cf10e5165ce940f84e6
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2017-04-04 06:47:57 +0300

    Configure db cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,4 +57,24 @@ RSpec.configure do |config|
   # Rspec macroses and helpers
   config.extend ControllerMacros, type: :controller
   config.include FeaturesHelpers, type: :feature
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
